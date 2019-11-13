f08929afc3a67cc06fc083b48372f74cf32f5200
Rob Gaston <rgaston@fargeo.com>
2019-11-13 12:06:10 -0800

    refactors

--- src/model.js
+++ src/model.js
@@ -18,11 +18,13 @@ Model.prototype.getData = function(req, callback) {
     const geometryType = geometryTypes[req.params.layer]
     const layerName = req.params.id
     const host = req.params.host
-    const qs = config.archesHosts[host].layers[layerName]
+    const qs = Object.assign({
+        'geometry_type': geometryType
+    }, config.archesHosts[host].layers[layerName])
     
     request({
-        url: `${config.archesHosts[host].url}/geojson?geometry_type=${geometryType}`,
-        qs: config.archesHosts[host].layers[layerName]
+        url: `${config.archesHosts[host].url}/geojson`,
+        qs: qs
     }, (err, res, geojson) => {
         if (err) return callback(err)
         
